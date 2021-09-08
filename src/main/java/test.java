697af225207bbac092510be1c8262a3c9e62928d
Elena Onica <onica.elena@ymail.com>
2021-09-08 21:46:57 +0300

    second change

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("BLA BLA");
         System.out.println("We did it!");
+        System.out.println("We did it!");
     }
 
 }
